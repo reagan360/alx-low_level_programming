0312670e9f7cd60d6508209903e7752d402df21e
reagan <reagan360mwenda@gmail.com>
2023-04-13 05:35:48 -0700

    a program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - program that prints its name, followed by a new line
+ *
+ * @argc: argument number
+ * @argv: argument number
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
